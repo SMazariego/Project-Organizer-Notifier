d40081f9fc6ffbdfda3392ec4767877a9b2a1582
gofoward-z <zlepeng.ucf@gmail.com>
2022-05-24 17:18:53 -0400

    uncheck box and clears the text

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
 
 $('input:checkbox').click(function(){
     var tb = "#"+$(this).attr('rel');
-    if($(this).is(":checked"))
-        $(tb).append(this.name + "\n");
-
-        else($(tb)).removeAttr(":checked");
+    let text_to_add = this.name + "\n";
+    //when click a checkbox and show checked items in the text area
+    if($(this).is(":checked")){
+        $(tb).append(text_to_add);
+    }else{
+        let remove = this.name +"\n";
+        //when a box is unchecked it clears the previously populated text from checkbox
+        $(tb).text(function(i, text){
+            return text .replace(text_to_add,'');
+        });
+    }
+        
 })
 
 
