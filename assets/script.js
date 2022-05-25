d4f0444a3151ad1c8a49bafba7b57e6de3ef9dd3
gofoward-z <zlepeng.ucf@gmail.com>
2022-05-25 17:04:29 -0400

    enterd text and checkbox text in same textfield

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,30 @@
 
+let existValue = "";
 $('input:checkbox').click(function(){
     var tb = "#"+$(this).attr('rel');
     let text_to_add = this.name + "\n";
+
+    let inputVal = $('#pname').val()
     //when click a checkbox and show checked items in the text area
     if($(this).is(":checked")){
-        $(tb).append(text_to_add);
+        $(tb).val($(tb).val() + text_to_add);
     }else{
         let remove = this.name +"\n";
         //when a box is unchecked it clears the previously populated text from checkbox
-        $(tb).text(function(i, text){
-            return text .replace(text_to_add,'');
-        });
+        $(tb).val($(tb).val().replace(remove,""));
     }
+
+    //storing the value to existValue
+
+    existValue = $(tb).val().replace(`${inputVal}\n`, "");
         
-})
+});
+
+$('#pname').on('input',(e)=>{
+    //here to adding the input value to the existValue
+    $('#textbox1').val(`${e.target.value}\n${existValue}`)
+});
+
 
 
 
