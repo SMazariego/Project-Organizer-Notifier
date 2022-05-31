6f2ef38185adc7067f838fa1e83ec28dc897bd1d
BPoole83 <102924794+BPoole83@users.noreply.github.com>
2022-05-31 19:48:57 -0400

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+<<<<<<< Updated upstream
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+=======
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  }
+>>>>>>> Stashed changes
